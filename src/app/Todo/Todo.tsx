9fc56d26d822bf5af1a56bfca5f43c7a1d7f2fc5
Ethan <ethan.mathieu@yale.edu>
2023-07-24 23:27:37 -0400

    handled error where the size of the todo was incorrect

--- src/app/Todo/Todo.tsx
+++ src/app/Todo/Todo.tsx
@@ -11,16 +11,36 @@ function Todo() {
   return (
     <div className=
     "bg-neutral-900 rounded-lg border-solid border-neutral-50 shadow-[#000000] shadow-md m-2 col-start-1 col-span-1 row-start-1 row-span-4 flex flex-col">
+      <h1 className="m-4 bg-neutral-800 text-white rounded-lg p-4 text-2xl font-extrabold text-center">
+        Todo
+      </h1>    
       <div className="row-start-1 row-span-3 grid grid-rows-3">
-
-          <h1 className="m-4 bg-neutral-800 text-white rounded-lg p-4 text-2xl font-extrabold text-center">
-            Todo
-          </h1>    
-
           <TodoItem 
             title={"Buy Groceries"}
             text={"This is a test to do item, let's see what happes when the text is longer!"}
           />  
+             <TodoItem 
+            title={"Buy Groceries"}
+            text={"This is a test to do item, let's see what happes when the text is longer!"}
+          />  
+          
+          <TodoItem 
+            title={"Buy Groceries"}
+            text={"This is a test to do item, let's see what happes when the text is longer!"}
+          />  
+             <TodoItem 
+            title={"Buy Groceries"}
+            text={"This is a test to do item, let's see what happes when the text is longer!"}
+          />     <TodoItem 
+          title={"Buy Groceries"}
+          text={"This is a test to do item, let's see what happes when the text is longer!"}
+        />     <TodoItem 
+        title={"Buy Groceries"}
+        text={"This is a test to do item, let's see what happes when the text is longer!"}
+      />     <TodoItem 
+      title={"Buy Groceries"}
+      text={"This is a test to do item, let's see what happes when the text is longer!"}
+    />  
           <TodoItem 
             title={"Buy Groceries"}
             text={"This is a test to do item, let's see what happes when the text is longer!"}
